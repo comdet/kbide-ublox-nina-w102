32149785823e91b746cf426c07235780857fd54a
comdet <listzone@hotmail.com>
2020-12-10 20:25:10 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {    
+    name : 'kbide-ublox-nina-w102',
+    platform : 'arduino-esp32',
+    title : 'U-BLOX NINA W120',
+    description : 'U-BLOX NINA W120.\n',
+    author : 'Comdet Phueadphut | partition by Miguel Alexandre Wisintainer',
+    website : 'https://docs.espressif.com/projects/esp-idf/en/latest/hw-reference/get-started-devkitc.html',
+    email : 'comdet.p@gmail.com',
+    git : 'https://github.com/comdet/kbide-ublox-nina-w102/',
+    image : '/static/EVK-NINA-W1-2CI.png',
+    version : '1.0.0'
+};
